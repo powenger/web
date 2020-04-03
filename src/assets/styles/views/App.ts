7a0057b6012cfecc2fcd888a576c2d07b0a93ec1
Mohammad Rajabloo <themrsoftware@gmail.com>
2020-04-04 00:31:33 +0430

    Add Material-ui to peoject with example (App.tsx)

---
+++ src/assets/styles/views/App.ts
@@ -0,0 +1,9 @@
+import { createStyles } from '@material-ui/core';
+
+export default createStyles({
+	title: {
+		width: '100%',
+		textAlign: 'center',
+		fontWeight: 'bold',
+	},
+});
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withStyles, WithStyles } from '@material-ui/styles';
+import style from '../assets/styles/views/App';
 
-const App = () => (
-	<div style={{ width: '100%', textAlign: 'center' }}>Hello World</div>
-);
+interface Props extends WithStyles<typeof style> {
 
-export default App;
+}
+
+class App extends Component<Props> {
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className={classes.title}>Hello World</div>
+		);
+	}
+}
+
+export default withStyles(style)(App);
